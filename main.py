25bf93fbbcc00df2b68528d5a157197a8c9918f0
Vaclav Pavlicek <vaclav@pavlicek.cz>
2015-12-04 14:17:45 +0100

    get indexes of searched pupils

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 def index_of_pupil(pupil, pupils):
     return pupils.find(pupil)
+
+
+def index_of_pupils_in_group(group, pupils):
+    result = []
+    for pupil in group:
+        result.append(pupils.find(pupil))
+        pupils = pupils[pupils.find(pupil):]
+    return result
--- test.py
+++ test.py
@@ -5,3 +5,6 @@ import main
 class TestBeforeMuseum(unittest.TestCase):
     def test_index_of_pupil(self):
         self.assertEquals(0, main.index_of_pupil('A', 'ABA'))
+
+    def test_index_of_pupils_in_group(self):
+        self.assertEquals([0, 1], main.index_of_pupils_in_group(['A', 'B'], 'ABA'))
