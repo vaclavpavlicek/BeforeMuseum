504805459ab53d331c2e4e8b4e49bd561dbc798e
Vaclav Pavlicek <vaclav@pavlicek.cz>
2015-12-04 14:28:17 +0100

    read group of searched students from input file

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def index_of_pupils_in_group(group, pupils):
         result.append(pupils.find(pupil))
         pupils = pupils[pupils.find(pupil):]
     return result
+
+
+def test_read_group_from_input_file(path_to_input_file):
+    input_file = open(path_to_input_file)
+    for x in range(0, 2):
+        group = input_file.readline()
+    return group
--- test.py
+++ test.py
@@ -8,3 +8,6 @@ class TestBeforeMuseum(unittest.TestCase):
 
     def test_index_of_pupils_in_group(self):
         self.assertEquals([0, 1], main.index_of_pupils_in_group(['A', 'B'], 'ABA'))
+
+    def test_read_group_from_input_file(self):
+        self.assertEquals('JAJ', main.test_read_group_from_input_file('input.txt'))
